2919cdd37b712eecc871449b8335978eff07c734
rasmusfabius <rasmusfabius@gmail.com>
2019-11-18 08:41:04 +0100

    Finished project

---
+++ httplibaryAsycAwait/app.js
@@ -0,0 +1,33 @@
+const http = new EasyHTTP();
+// GET Users
+
+http
+  .get('https://jsonplaceholder.typicode.com/users')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+// User Data
+
+const data = {
+  name: 'Rasmus Meinecke',
+  username: 'rasmusM',
+  email: 'rasmus@test.nu'
+};
+// Create Users
+http
+  .post('https://jsonplaceholder.typicode.com/users', data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+// Update Users
+http
+  .put('https://jsonplaceholder.typicode.com/users/2', data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+// Delete Users
+
+http
+  .delete('https://jsonplaceholder.typicode.com/users/2')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
